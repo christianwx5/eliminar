8132275a3609047d0d97311e22b7202323f4c411
christianwx5 <79468653+christianwx5@users.noreply.github.com>
2022-02-01 21:38:21 -0400

    reas

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,24 @@
 import express, { Application, Request, Response } from "express";
 
+
+import "reflect-metadata";
+import {createConnection} from "typeorm";
+import * as bodyParser from "body-parser";
+import {Routes} from "./routes";
+import {Socios} from "./entity/Socios";
+
+
+import  cors from 'cors';
+import * as helmet from 'helmet';
+
+
+
 const app: Application = express();
 
 app.set("port", process.env.PORT || 3000);
 
 app.get("/", (_req: Request, res: Response) => {
-  res.json({ message: "Hello world! mercoles 2" });
+  res.json({ message: "Hello world! mercoles 3" });
 });
 
 app.listen(app.get("port"), () => {
